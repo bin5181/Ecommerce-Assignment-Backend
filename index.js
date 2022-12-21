5a12be237a16b6a7d49f837d104f1f62e08523c5
Bhanu Prakash Bindal <93444297+bin5181@users.noreply.github.com>
2022-12-21 21:06:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // To use cors
-// app.use(cors());
+app.use(cors({origin: ["http://localhost:3000/", "https://ecommerce-assignment-frontend.vercel.app/"];));
 
 // To use json bodies
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   next();
+// })
 
 // Available Routes
 app.use("/auth", require("./Routes/auth"));
