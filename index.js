4466502a6c551eee0f4c71aca52e0c82ad66aa5a
Bhanu Prakash Bindal <93444297+bin5181@users.noreply.github.com>
2022-12-21 21:03:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  next();
 })
 
 // Available Routes
